29b89e554bdace346e4449dfbcfa8f584222a2e9
jimzhang629 <46616647+jimzhang629@users.noreply.github.com>
2019-03-07 10:31:12 -0600

    Update MakeEverythingWork.ino

--- MakeEverythingWork.ino
+++ MakeEverythingWork.ino
@@ -15,17 +15,17 @@ int adc_key_in = 0;
 // read the buttons
 int read_LCD_buttons()
 {
- adc_key_in = analogRead(0); // read the value from the sensor
- // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
- // we add approx 50 to those values and check to see if we are close
- if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
- // For V1.1 use this threshold
- if (adc_key_in < 50) return btnRIGHT;
- if (adc_key_in < 250) return btnUP;
- if (adc_key_in < 450) return btnDOWN;
- if (adc_key_in < 650) return btnLEFT;
- if (adc_key_in < 850) return btnSELECT;
- return btnNONE; // when all others fail, return this…
+  adc_key_in = analogRead(0); // read the value from the sensor
+  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+  // we add approx 50 to those values and check to see if we are close
+  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  // For V1.1 use this threshold
+  if (adc_key_in < 50) return btnRIGHT;
+  if (adc_key_in < 250) return btnUP;
+  if (adc_key_in < 450) return btnDOWN;
+  if (adc_key_in < 650) return btnLEFT;
+  if (adc_key_in < 850) return btnSELECT;
+  return btnNONE; // when all others fail, return this…
 }
 
 // Define LCD pins
@@ -62,38 +62,38 @@ void loop() {
   switch (lcd_key) // depending on which button was pushed, we perform an action
   {
     
-  case btnRIGHT:
-  {
-  
-  lcd.setCursor(0,1);
-  lcd.print("RIGHT ");
-  
-  while(lcd_key != btnUP and lcd_key!= btnDOWN and lcd_key != btnLEFT){ // if btn isn't up, down, or left, then spin a revolution right
-    myMotor->step(200, FORWARD, DOUBLE); //step the number of revolutions you want
-    rev_right = rev_right + 1; // increment the counter
-    lcd.setCursor(7,1);
-    lcd.print(rev_right); 
-    lcd_key = read_LCD_buttons(); //check for if the btn has changed
-  }
-  myMotor->release();// release all power from the motor 
-  break;
-  
-  }
-  
-  case btnLEFT:
-  {
-  lcd.setCursor(0,0);
-  lcd.print("LEFT ");
-  while(lcd_key != btnUP and lcd_key != btnDOWN and lcd_key != btnRIGHT){ //same code but for left revolution
-    myMotor->step(200, BACKWARD, DOUBLE);
-    rev_left = rev_left + 1;
-    lcd.setCursor(7,0);
-    lcd.print(rev_left);
-    lcd_key = read_LCD_buttons();
-  }
-  myMotor->release();
-  break;
-  }
+    case btnRIGHT:
+    {
+
+    lcd.setCursor(0,1);
+    lcd.print("RIGHT ");
+
+    while(lcd_key != btnUP and lcd_key!= btnDOWN and lcd_key != btnLEFT){ // if btn isn't up, down, or left, then spin a revolution right
+      myMotor->step(200, FORWARD, DOUBLE); //step the number of revolutions you want
+      rev_right = rev_right + 1; // increment the counter
+      lcd.setCursor(7,1);
+      lcd.print(rev_right); 
+      lcd_key = read_LCD_buttons(); //check for if the btn has changed
+    }
+    myMotor->release();// release all power from the motor 
+    break;
+
+    }
+
+    case btnLEFT:
+    {
+    lcd.setCursor(0,0);
+    lcd.print("LEFT ");
+    while(lcd_key != btnUP and lcd_key != btnDOWN and lcd_key != btnRIGHT){ //same code but for left revolution
+      myMotor->step(200, BACKWARD, DOUBLE);
+      rev_left = rev_left + 1;
+      lcd.setCursor(7,0);
+      lcd.print(rev_left);
+      lcd_key = read_LCD_buttons();
+    }
+    myMotor->release();
+    break;
+    }
   
 }
 }
